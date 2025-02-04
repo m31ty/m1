900de9e10bc2a04da61db4b5d7c6345c40cf4bb9
m31ty <m31tyv@gmail.com>
2025-02-04 22:56:23 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # 更新と ffmpeg のインストール
 echo "Updating package list and installing ffmpeg..."
-sudo apt update && sudo apt install -y ffmpeg
+apt update && apt install -y ffmpeg
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
