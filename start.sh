7d90f18e397d73ec723b9a645772fd5d468944a7
m31ty <m31tyv@gmail.com>
2025-02-05 13:06:20 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ apt update && apt install -y ffmpeg
 pip install -r requirements.txt
 
 # ZIPファイルのダウンロードと展開処理
-wget -O poop.zip https://drive.usercontent.google.com/download?id=1gcmz3wDf2WkEfD_eAmvGHz0o940E3RGJ&export=download&authuser=0&confirm=t&uuid=84e3c29d-0139-4728-8390-ee12c8e6d9a6&at=AIrpjvPxB0MFb13M_qTdgFK-CWki%3A1738727798019
+wget -O poop.zip "https://drive.usercontent.google.com/download?id=1gcmz3wDf2WkEfD_eAmvGHz0o940E3RGJ&confirm=t&uuid=732ba3b9-38dd-47da-b8f3-b5a621eac06d&at=AIrpjvOOuxT2yvn6NDga3MD5Syjf%3A1738728329982"
 unzip poop.zip
 
 # poopディレクトリの中身をmoviesに移動
