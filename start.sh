a5ea272917a2fe14df378a3a569336fae276020d
m31ty <m31tyv@gmail.com>
2025-02-05 13:25:26 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,30 +4,30 @@
 apt update && apt install -y ffmpeg
 
 pip install -r requirements.txt
-echo "HI, m3th!."
-ls
-df -h --total
 
-# ZIPファイルのダウンロードと展開処理
-wget -O poop.zip "https://drive.usercontent.google.com/download?id=1gcmz3wDf2WkEfD_eAmvGHz0o940E3RGJ&confirm=t&uuid=732ba3b9-38dd-47da-b8f3-b5a621eac06d&at=AIrpjvOOuxT2yvn6NDga3MD5Syjf%3A1738728329982"
+URL="https://drive.usercontent.google.com/download?id=1gcmz3wDf2WkEfD_eAmvGHz0o940E3RGJ&confirm=t&uuid=732ba3b9-38dd-47da-b8f3-b5a621eac06d&at=AIrpjvOOuxT2yvn6NDga3MD5Syjf%3A1738728329982"
+
+# 保存先のファイル名
+ZIP_FILE="poop.zip"
+
+# 展開先のフォルダ
+DEST_DIR="movies"
+
+# zipをダウンロード
+wget "$URL" -O "$ZIP_FILE"
 echo "DL complete."
-ls
-df -h --total
 
-unzip poop.zip
+# moviesフォルダがない場合は作成
+mkdir -p "$DEST_DIR"
+
+# zipを解凍（poopフォルダの中身のみをmoviesに移動）
+unzip -o "$ZIP_FILE" -d "tmp_poop"
+mv tmp_poop/poop/* "$DEST_DIR"/
 echo "unzip complete."
-ls
-df -h --total
 
-
-# poopディレクトリの中身をmoviesに移動
-mkdir -p movies
-cp -r poop/. movies/
-
-# 不要なファイルを削除
-rm -rf poop.zip poop
+# 一時フォルダとzipファイルを削除
+rm -rf tmp_poop "$ZIP_FILE"
 echo "delete file."
-ls
 df -h --total
 
 echo "Setup complete."
