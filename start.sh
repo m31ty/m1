2667ec22b1439c3bb6351d1b879efd0d632e83c2
m31ty <m31tyv@gmail.com>
2025-02-05 12:38:21 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+apt update && apt install -y ffmpeg
+
 # ZIPファイルのダウンロードと展開処理
 wget -O poop.zip "https://drive.usercontent.google.com/download?id=1An7g4W538jGbeEuX8Vf1z-pU8UDMwS6Y&export=download&authuser=0&confirm=t&uuid=e1ab994f-79d4-43df-8a80-d32bb7c22991&at=AIrpjvOlBDqjncW5WAWODRK5xiNB%3A1738724788247"
 unzip poop.zip
@@ -11,8 +14,6 @@ cp -r poop/. movies/
 # 不要なファイルを削除
 rm -rf poop.zip poop
 
-# パッケージと依存関係のインストール
-apt update && apt install -y ffmpeg
 pip install -r requirements.txt
 
 echo "Setup complete."
