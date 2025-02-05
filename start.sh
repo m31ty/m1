b02706cebc031156fee50aaa9e3e6b9315dc6aa0
m31ty <m31tyv@gmail.com>
2025-02-05 12:37:39 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-# 必要なパッケージのインストール
+# ZIPファイルのダウンロードと展開処理
+wget -O poop.zip "https://drive.usercontent.google.com/download?id=1An7g4W538jGbeEuX8Vf1z-pU8UDMwS6Y&export=download&authuser=0&confirm=t&uuid=e1ab994f-79d4-43df-8a80-d32bb7c22991&at=AIrpjvOlBDqjncW5WAWODRK5xiNB%3A1738724788247"
+unzip poop.zip
+
+# poopディレクトリの中身をmoviesに移動
+mkdir -p movies
+cp -r poop/. movies/
+
+# 不要なファイルを削除
+rm -rf poop.zip poop
+
+# パッケージと依存関係のインストール
 apt update && apt install -y ffmpeg
 pip install -r requirements.txt
-
-# poop.zip をダウンロード
-wget -O poop.zip "https://drive.usercontent.google.com/download?id=1An7g4W538jGbeEuX8Vf1z-pU8UDMwS6Y&export=download&authuser=0&confirm=t&uuid=e1ab994f-79d4-43df-8a80-d32bb7c22991&at=AIrpjvOlBDqjncW5WAWODRK5xiNB%3A1738724788247"
-
-
-# poop.zip を解凍
-tmp_dir=$(mktemp -d)
-unzip poop.zip -d "$tmp_dir"
-
-# movies フォルダが存在しない場合は作成
-mkdir -p movies
-
-# poop フォルダの中身を movies に移動
-mv "$tmp_dir/poop"/* movies/
-
-# 一時ディレクトリの削除
-rm -rf "$tmp_dir"
-rm poop.zip
 
 echo "Setup complete."
