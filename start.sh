505dceb8ec01ac1aa8e56042d181da360013345b
m31ty <m31tyv@gmail.com>
2025-02-05 14:06:17 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ apt update && apt install -y ffmpeg
 pip install -r requirements.txt
 
 
-wget -O poop.zip "https://drive.usercontent.google.com/download?id=1_iE7rw2YXrM-yEL64NfWgxggzNo5HAAP&export=download&authuser=0&confirm=t&uuid=2e3f5d80-ebf2-45b3-86c3-36359123b7f2&at=AIrpjvOsCVliS5jie6aGtF5swjpx%3A1738730344270"
+wget -O poop.zip "https://uc279c65fa251cda0e9a9b21c90d.dl.dropboxusercontent.com/cd/0/get/CjgB5vXhZFZPDi6TPcqv_bPq0UHlVozuVXaqxUjyvNRy6_xJGsUBBFrMDyU4-W0VVsdzq9-zyFcKTiwd7s1BJJ9hoqt3PEnhu-wPfK7Xo9lcElKBfNuK9laxiBbuF4eeAd8-a37rE4Dp0TVg3tinAzQgd8CNZVEaSlDBIZziOkauFA/file?_download_id=3023850992775234462463448572417535957188091852228673603674103094&_log_download_success=1#"
 echo "DL complete."
 ls
 unzip poop.zip
