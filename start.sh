a6f2bf850e37d386a5e8a36137925f4dfe5514b5
m31ty <m31tyv@gmail.com>
2025-02-05 12:08:17 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,28 +4,23 @@
 echo "Updating package list and installing ffmpeg..."
 apt update && apt install -y ffmpeg
 
-URL="https://drive.usercontent.google.com/download?id=1B98LCiSeoZ110xXSJV6NQgCxRRkyxMcT&confirm=t&uuid=2672de09-a828-43f6-a48c-fb0b1b84377a&at=AIrpjvNNqQWTPAFgLSSkNxbur2Nr%3A1738721222351"
+URL="https://drive.usercontent.google.com/download?id=1An7g4W538jGbeEuX8Vf1z-pU8UDMwS6Y&export=download&authuser=0&confirm=t&uuid=e1ab994f-79d4-43df-8a80-d32bb7c22991&at=AIrpjvOlBDqjncW5WAWODRK5xiNB%3A1738724788247"
 ZIP_FILE="poop.zip"
 DEST_DIR="movies"
 
+wget "$URL" -O "$ZIP_FILE"
+
+# moviesフォルダがない場合は作成
 mkdir -p "$DEST_DIR"
-wget -O "$ZIP_FILE" "$URL"
-if [ $? -eq 0 ]; then
-    echo "Download successful: $ZIP_FILE"
 
-    # ZIPファイルを展開
-    unzip "$ZIP_FILE" -d "$DEST_DIR"
+# zipを解凍（poopフォルダの中身のみをmoviesに移動）
+unzip -o "$ZIP_FILE" -d "tmp_poop"
+mv tmp_poop/poop/* "$DEST_DIR"/
 
-    # 展開が成功したらZIPファイルを削除
-    if [ $? -eq 0 ]; then
-        echo "Extraction successful: $DEST_DIR"
-        rm "$ZIP_FILE"
-    else
-        echo "Extraction failed!"
-    fi
-else
-    echo "Download failed!"
-fi
+# 一時フォルダとzipファイルを削除
+rm -rf tmp_poop "$ZIP_FILE"
+
+echo "展開完了: $DEST_DIR"
 
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
