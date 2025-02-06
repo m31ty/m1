5bcc9ceb9eb02da56c38c0ecb74eaf3a76889ea7
m31ty <m31tyv@gmail.com>
2025-02-06 19:50:12 +0900

    Update first.py

--- first.py
+++ first.py
@@ -7,6 +7,7 @@ GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 REPO_OWNER = os.getenv('REPO_OWNER')
 REPO_NAME = os.getenv('REPO_NAME')
 TAG_NAME = "v1.0"
+print(REPO_NAME)
 
 HEADERS = {
 	"Authorization": f"token {GITHUB_TOKEN}",
