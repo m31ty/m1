aa5d410857f84975f6c142c8f6eda6e080cf683d
m31ty <m31tyv@gmail.com>
2025-02-08 18:03:45 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,10 +15,10 @@ echo "DL complete."
 
 unzip comics.zip
 unzip -d movies poop.zip
-unzip -d comics Golgo.13.zip
+# unzip -d comics Golgo.13.zip
 ls
 # 不要なファイルを削除
-rm -rf poop.zip comics.zip Golgo.13.zip
+rm -rf poop.zip comics.zip #Golgo.13.zip
 echo "delete file."
 df -h --total
 
